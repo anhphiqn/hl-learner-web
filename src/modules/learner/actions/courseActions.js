98a9f5c08e4840462b8c55af6745220aecea20b4
Tuan Dang <tuan.dang@ibm.com>
2018-08-26 10:15:01 -0700

    update

---
+++ src/modules/learner/actions/courseActions.js
@@ -0,0 +1,225 @@
+import axios from 'axios';
+
+import { GET_ERRORS } from '../../common/types';
+
+import {
+  GET_COURSE,
+  DELETE_COURSE,
+  GET_COURSES,
+  COURSE_LOADING,
+} from '../types';
+
+// Get Course
+export const getCourse = (id, userEmail) => dispatch => {
+  dispatch(setCourseLoading());
+
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL;
+  const url = `${serviceUrl}/course/${id}/${userEmail}`; 
+
+  axios
+    .get(url)
+    .then(res =>{
+      dispatch({
+        type: GET_COURSE,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_COURSE,
+        payload: null
+      })
+    );
+};
+// Add course
+export const addCourse = (courseData, callback) => dispatch => {
+  dispatch(setCourseLoading());
+
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL;
+  const url = `${serviceUrl}/course`; 
+  console.log(url);
+  axios
+    .post(url, courseData)
+    .then(res =>{
+      dispatch({
+        type: GET_COURSE,
+        payload: res.data
+      });
+      callback();
+    })
+    .catch(err => {
+      console.log('addCourse error ', err);
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+    );
+};
+
+// enrollCourse
+export const enrollCourse = (courseEnrollData, callback) => dispatch => {
+  dispatch(setCourseLoading());
+
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL;
+  const url = `${serviceUrl}/course/enroll`; 
+  console.log(url);
+  axios
+    .post(url, courseEnrollData)
+    .then(res =>{
+      dispatch({
+        type: GET_COURSE,
+        payload: res.data
+      });
+      callback();
+    })
+    .catch(err => {
+      console.log('enrollCourse error ', err);
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+    );
+};
+
+// enrollCourse
+export const unEnrollCourse = (courseUnEnrollData, callback) => dispatch => {
+  dispatch(setCourseLoading());
+
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL;
+  const url = `${serviceUrl}/course/unenroll`; 
+  console.log(url);
+  axios
+    .post(url, courseUnEnrollData)
+    .then(res =>{
+      dispatch({
+        type: GET_COURSE,
+        payload: res.data
+      });
+      callback();
+    })
+    .catch(err => {
+      console.log('unEnrollCourse error ', err);
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+    );
+};
+
+// Delete Course
+export const deleteCourse = (id, userEmail) => dispatch => {
+  var answer = window.confirm('Are you sure to delete this Course? This can NOT be undone!');
+  if(!answer)
+    return;  
+  
+    const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL;
+    const url = `${serviceUrl}/course/${id}/${userEmail}`; 
+
+    axios
+    .delete(url)
+    .then(res =>{
+      console.log('deleteCourse');
+      console.log(id);
+      dispatch({
+        type: DELETE_COURSE,
+        payload: id
+      });
+    })
+    .catch(err => {
+      console.log('deleteCourse err');
+      console.log(err);
+      
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+    );
+};
+
+// Search courses
+export const getCourses = (searchTerm, userEmail) => dispatch => {
+  dispatch(setCourseLoading());
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL
+  searchTerm = !searchTerm ? '*' : searchTerm; 
+  const url = `${serviceUrl}/course/search/${searchTerm}/${userEmail}`;
+  console.log(url);
+ 
+  axios
+    .get(url)
+    .then(res =>
+      {
+        dispatch({
+          type: GET_COURSES,
+          payload: res.data
+        });  
+      }
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_COURSES,
+        payload: null
+      })
+    );
+};
+
+// Search unenrolled courses
+export const getUnEnrolledCourses = (searchTerm, learnerId, userEmail) => dispatch => {
+  dispatch(setCourseLoading());
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL
+  searchTerm = !searchTerm ? '*' : searchTerm; 
+  const url = `${serviceUrl}/course/searchUnEnrolled/${searchTerm}/${learnerId}/${userEmail}`;
+  console.log(url);
+ 
+  axios
+    .get(url)
+    .then(res =>
+      {
+        dispatch({
+          type: GET_COURSES,
+          payload: res.data
+        });  
+      }
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_COURSES,
+        payload: null
+      })
+    );
+};
+
+// Search enrolled courses
+export const getEnrolledCourses = (searchTerm, learnerId, userEmail) => dispatch => {
+  dispatch(setCourseLoading());
+  const serviceUrl = process.env.REACT_APP_COURSE_SERVICE_URL
+  searchTerm = !searchTerm ? '*' : searchTerm; 
+  const url = `${serviceUrl}/course/searchEnrolled/${searchTerm}/${learnerId}/${userEmail}`;
+  console.log(url);
+ 
+  axios
+    .get(url)
+    .then(res =>
+      {
+        dispatch({
+          type: GET_COURSES,
+          payload: res.data
+        });  
+      }
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_COURSES,
+        payload: null
+      })
+    );
+};
+// Course loading
+export const setCourseLoading = () => {
+  return {
+    type: COURSE_LOADING
+  };
+};
